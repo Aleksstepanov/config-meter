d7d1af45df78a566f5c8a935d0b075d71a9b63d5
Aleksey <stepanovl87@gmail.com>
2021-07-21 16:49:15 +0300

    navbar and sidebar

---
+++ src/components/NavBar.vue
@@ -0,0 +1,56 @@
+<template>
+  <b-navbar toggleable="lg" type="dark" variant="info" fluid>
+    <b-row>
+      <b-col sm="1">
+        <SideBar />
+      </b-col>
+      <b-col sm="4">
+        <b-navbar-brand href="#">
+          <img src="@/assets/logo.png" alt="logo" />
+        </b-navbar-brand>
+      </b-col>
+      <b-col sm="4" offset-sm="3">
+        <b-navbar-nav class="ml-auto">
+          <b-nav-form>
+            <b-form-input
+              size="sm"
+              class="mr-sm-2"
+              placeholder="Search"
+            ></b-form-input>
+            <b-button size="sm" class="my-2 my-sm-0" type="submit"
+              >Search</b-button
+            >
+          </b-nav-form>
+
+          <b-nav-item-dropdown text="Lang" right>
+            <b-dropdown-item href="#">EN</b-dropdown-item>
+            <b-dropdown-item href="#">ES</b-dropdown-item>
+            <b-dropdown-item href="#">RU</b-dropdown-item>
+            <b-dropdown-item href="#">FA</b-dropdown-item>
+          </b-nav-item-dropdown>
+
+          <b-nav-item-dropdown right>
+            <!-- Using 'button-content' slot -->
+            <template #button-content>
+              <em>User</em>
+            </template>
+            <b-dropdown-item href="#">Profile</b-dropdown-item>
+            <b-dropdown-item href="#">Sign Out</b-dropdown-item>
+          </b-nav-item-dropdown>
+        </b-navbar-nav>
+      </b-col>
+    </b-row>
+  </b-navbar>
+</template>
+
+<script>
+import SideBar from "@/components/SideBar.vue";
+
+export default {
+  components: {
+    SideBar,
+  },
+};
+</script>
+
+<style></style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,19 +1,18 @@
 <template>
-  <div class="home">
-    <h1>Hallo</h1>
-    <div>
-      <b-button>Button</b-button>
-      <b-button variant="danger">Button</b-button>
-      <b-button variant="success">Button</b-button>
-      <b-button variant="outline-primary">Button</b-button>
-    </div>
+  <div>
+    <b-row>
+      <NavBar />
+    </b-row>
   </div>
 </template>
 
 <script>
-// @ is an alias to /src
+import NavBar from "@/components/NavBar.vue";
 
 export default {
   name: "Home",
+  components: {
+    NavBar,
+  },
 };
 </script>
